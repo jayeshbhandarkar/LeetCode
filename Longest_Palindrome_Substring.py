fd858e10712dbc71b5c3f4da8b9959c9bc5d7800
Jayesh Bhandarkar <bhandarkarjayesh721@gmail.com>
2025-01-21 23:09:39 +0530

    Create Longest_Palindrome_Substring.py

---
+++ Longest_Palindrome_Substring.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        res = ""
+        
+        def findLongestPalindrome(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
+        
+        for i in range(len(s)):
+            # odd-length palindrome
+            odd_palindrome = findLongestPalindrome(i, i)
+            if len(odd_palindrome) > len(res):
+                res = odd_palindrome
+
+            # even-length palindrome
+            even_palindrome = findLongestPalindrome(i, i + 1)
+            if len(even_palindrome) > len(res):
+                res = even_palindrome
+        
+        return res
+
+solution = Solution()
