e3fa830e615d9f2fed960d56875b7207d2cd52c1
Jayesh Bhandarkar <bhandarkarjayesh721@gmail.com>
2025-01-26 17:52:13 +0530

    Create Merge_Two_Sorted_Lists.py

---
+++ Merge_Two_Sorted_Lists.py
@@ -0,0 +1,46 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        dummy = ListNode()
+        current = dummy
+        
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+
+        if list1:
+            current.next = list1
+        elif list2:
+            current.next = list2
+        
+        return dummy.next 
+
+def createLinkedList(values):
+    dummy = ListNode()
+    current = dummy
+    for val in values:
+        current.next = ListNode(val)
+        current = current.next
+    return dummy.next
+
+def printLinkedList(head):
+    while head:
+        print(head.val, "->")
+        head = head.next
+    print("None")
+
+list1 = createLinkedList([1, 3, 5])
+list2 = createLinkedList([2, 4, 6])
+
+s1 = Solution()
+mergedList = s1.mergeTwoLists(list1, list2)
+printLinkedList(mergedList)
