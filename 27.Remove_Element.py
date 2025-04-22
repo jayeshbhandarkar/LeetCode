13db97676df519bf53c5d0c137b6ec869187a3ee
Jayesh Bhandarkar <bhandarkarjayesh721@gmail.com>
2025-04-22 19:44:42 +0530

    Update and rename Remove_Element.py to 27.Remove_Element.py

--- 27.Remove_Element.py
+++ 27.Remove_Element.py
@@ -7,13 +7,3 @@ class Solution(object):
                 k += 1 
     
         return k
-
-s1 = Solution()
-nums1 = [3, 2, 2, 3]
-val1 = 3
-
-nums2 = [0, 1, 2, 2, 3, 0, 4, 2]
-val2 = 2
-
-print(s1.removeElement(nums1, val1)) 
-print(s1.removeElement(nums2, val2))  
