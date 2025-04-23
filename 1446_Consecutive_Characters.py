0772a702bcabe89ddfc431978c15cfce91fb86d1
Jayesh Bhandarkar <bhandarkarjayesh721@gmail.com>
2025-04-23 23:07:28 +0530

    Create 1446_Consecutive_Characters.py

---
+++ 1446_Consecutive_Characters.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def maxPower(self, s):
+        max_count = count = 1
+
+        for i in range(1, len(s)):
+            if s[i] == s[i-1]:
+                count += 1
+                max_count = max(max_count, count)
+
+            else:
+                count = 1
+
+        return max_count       
