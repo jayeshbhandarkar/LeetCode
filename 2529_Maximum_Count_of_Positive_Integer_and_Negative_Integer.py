89ba4ecbe11bba4a66236a956764fd019a09fec4
Jayesh Bhandarkar <bhandarkarjayesh721@gmail.com>
2025-04-23 23:28:54 +0530

    Create 2529_Maximum_Count_of_Positive_Integer_and_Negative_Integer.py

---
+++ 2529_Maximum_Count_of_Positive_Integer_and_Negative_Integer.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maximumCount(self, nums):
+        neg_count = 0
+        pos_count = 0
+        
+        for i in nums:
+            if i > 0:
+                pos_count += 1
+
+            elif i < 0:
+                neg_count += 1
+
+        return max(pos_count,neg_count)
+        
