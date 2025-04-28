65d527d5df2bfed9e99c15a70bb3482a985b295a
Jayesh Bhandarkar <bhandarkarjayesh721@gmail.com>
2025-04-28 11:42:23 +0530

    Create 2302_Count_Subarrays_With_Score_Less_Than_K.py

---
+++ 2302_Count_Subarrays_With_Score_Less_Than_K.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def countSubarrays(self, nums, k):
+        count = 0
+        left = 0
+        sum = 0
+
+        for right in range(len(nums)):
+            sum += nums[right]
+
+            while left <= right and sum * (right - left + 1) >= k:
+                sum -= nums[left]
+                left += 1
+
+            count += (right - left + 1) 
+
+        return count
+          
